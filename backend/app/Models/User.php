bcbaecb5c0210c42592e1d68889cb8849fc92ecf
Kali07 <richardmulamba16@gmail.com>
2026-07-24 14:19:07 +0200

    update userModel + migration erreur tocken

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -7,8 +7,10 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
 
@@ -48,5 +50,15 @@ class User extends Authenticatable
     public function agendas(): HasMany
     {
         return $this->hasMany(Agenda::class, 'created_by');
+    }
+
+    public function getNameAttribute(): string
+    {
+        return trim("{$this->prenom} {$this->nom}");
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->actif;
     }
 }
