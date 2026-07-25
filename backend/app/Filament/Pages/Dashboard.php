f2b687a6adc3e02775ce7149028ea48ce3bf088b
Kali07 <richardmulamba16@gmail.com>
2026-07-25 22:04:23 +0200

    initialisation dashboard

---
+++ backend/app/Filament/Pages/Dashboard.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use App\Filament\Widgets\EventsChartWidget;
+use App\Filament\Widgets\LatestBooksWidget;
+use App\Filament\Widgets\LatestEventsWidget;
+use App\Filament\Widgets\LatestTeachingsWidget;
+use App\Filament\Widgets\StatsOverviewWidget;
+use App\Filament\Widgets\TodayAgendaWidget;
+use Filament\Pages\Dashboard as BaseDashboard;
+
+class Dashboard extends BaseDashboard
+{
+    protected static ?string $title = 'Tableau de bord';
+
+    public function getWidgets(): array
+    {
+        return [
+            StatsOverviewWidget::class,
+            EventsChartWidget::class,
+            TodayAgendaWidget::class,
+            LatestEventsWidget::class,
+            LatestTeachingsWidget::class,
+            LatestBooksWidget::class,
+        ];
+    }
+}
