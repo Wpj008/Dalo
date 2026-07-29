b3ab476f1c0128eae57ecc6421457d2e1dc6e63b
Kali07 <richardmulamba16@gmail.com>
2026-07-30 01:01:25 +0200

    finalisation routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\AgendaController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\TeachingController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -16,3 +18,13 @@ Route::get('/events/{event}', [EventController::class, 'show']);
 
 Route::get('/agendas', [AgendaController::class, 'index']);
 Route::get('/agendas/{agenda}', [AgendaController::class, 'show']);
+
+//Livres
+
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/books/{book}', [BookController::class, 'show']);
+
+
+//Enseignements
+Route::get('/teachings', [TeachingController::class, 'index']);
+Route::get('/teachings/{teaching}', [TeachingController::class, 'show']);
